1c93d53386f0309e41cb1477a2b42639f195569a
Javier Vargas <javierralejandroortga@gmail.com>
2023-09-14 13:04:22 -0400

    Create tests for factorial method

---
+++ spec/solver_rspec.rb
@@ -0,0 +1,13 @@
+require_relative '../solver'
+
+describe Solver do
+  subject { Solver.new() }
+
+  it 'should return the factorial of 6' do
+    expect(subject.factorial(6)).to eq(720)
+  end
+
+  it 'should return the factorial of 5' do
+    expect(subject.factorial(6)).to eq(720)
+  end
+end
